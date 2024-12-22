127e209cce8f9320701d70106493c30e30359642
multifeb13 <musicfair@gmail.com>
2024-12-22 23:08:52 +0900

    feat: change LED lighting pattern

--- keyboards/multifeb13/proto3g/keymaps/default/keymap.c
+++ keyboards/multifeb13/proto3g/keymaps/default/keymap.c
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
+#include <math.h>
 
 enum layer_number {
     _BASE = 0,
@@ -31,16 +32,25 @@ void matrix_init_user(void) {
 void matrix_scan_user(void) {
     static int counter = 0;
     static int index = 0;
+    static int degree = 0;
 
-    counter++;
-
-    if (counter > 10000) {
+    if (counter >= 150){
         counter = 0;
-        index++;
+        degree++;
 
-        rgblight_sethsv_at(   0, 255, 255, (index + 0) % 4);
-        rgblight_sethsv_at( 170, 255, 255, (index + 1) % 4);
-        rgblight_sethsv_at(  85, 255, 255, (index + 2) % 4);
-        rgblight_sethsv_at(  43, 255, 255, (index + 3) % 4);
+        double radian = degree * (M_PI / 180.0);
+        double sin_value = sin(radian);
+        unsigned int led_v = 128 * sin_value;
+
+        rgblight_sethsv_at(   0, 255, led_v, (index + 0) % 4);
+        rgblight_sethsv_at( 170, 255, led_v, (index + 1) % 4);
+        rgblight_sethsv_at(  85, 255, led_v, (index + 2) % 4);
+        rgblight_sethsv_at(  43, 255, led_v, (index + 3) % 4);
+
+        if(degree >= 180){
+          degree = 0;
+          index = (index + 1) % 4;
+        }
     }
+    counter++;
 }
