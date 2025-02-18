e2ac2711dbdcd30fbf5efcd1c7a7e2cf22c6fb81
multifeb13 <musicfair@gmail.com>
2025-02-18 22:10:51 +0900

    fix: key assign

--- keyboards/multifeb13/prac_xiao2/keymaps/default/keymap.c
+++ keyboards/multifeb13/prac_xiao2/keymaps/default/keymap.c
@@ -5,13 +5,9 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_ESCAPE,      KC_Q,        KC_W,        KC_E,           KC_R,         KC_T,     KC_Y,
-        KC_U,           KC_I,        KC_O,        KC_P,           KC_MINUS,
-        KC_TAB,         KC_A,        KC_S,        KC_D,           KC_F,         KC_G,     KC_H,
-        KC_J,           KC_K,        KC_L,        KC_SEMICOLON,   KC_BACKSPACE,
-        KC_LEFT_SHIFT,  KC_Z,        KC_X,        KC_C,           KC_V,         KC_B,     KC_N,
-        KC_M,           KC_COMMA,    KC_DOT,      KC_RIGHT_SHIFT, KC_NO,
-        KC_LEFT_CTRL,   KC_LEFT_GUI, KC_LEFT_ALT, KC_NO,          KC_NO,        KC_ENTER, KC_LEFT,
-        KC_DOWN,        KC_UP,       KC_RIGHT,    KC_DELETE
+        KC_TAB,         KC_Q,        KC_W,        KC_E,           KC_R,         KC_T,     KC_Y,         KC_U,           KC_I,        KC_O,        KC_P,           KC_GRAVE,
+        KC_LEFT_CTRL,   KC_A,        KC_S,        KC_D,           KC_F,         KC_G,     KC_H,         KC_J,           KC_K,        KC_L,        KC_SEMICOLON,   KC_ENTER,
+        KC_LEFT_SHIFT,  KC_Z,        KC_X,        KC_C,           KC_V,         KC_B,     KC_N,         KC_M,           KC_COMMA,    KC_DOT,      KC_RIGHT_SHIFT, KC_RIGHT_SHIFT,
+        KC_NO,          KC_LEFT_ALT, KC_LEFT_GUI, KC_SPACE,       KC_NO,        KC_SPACE, KC_LEFT_ALT,  KC_DOWN,        KC_UP,       KC_RIGHT,    KC_BACKSPACE
     )
 };
