4c2763e70a75900be3d6553fc8b7f47d5828a452
multifeb13 <musicfair@gmail.com>
2025-05-11 15:56:41 +0900

    feat: modify key map

--- keyboards/multifeb13/prac6/keymaps/default/keymap.c
+++ keyboards/multifeb13/prac6/keymaps/default/keymap.c
@@ -16,9 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───┴───┴───┘
      */
     [0] = LAYOUT(
-        KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
-        KC_P4,   KC_P5,   KC_P6,   KC_PAST,
-        KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
-        KC_P0,   KC_PDOT, KC_PENT, KC_PPLS
+        LALT(KC_A), LCTL(KC_D), KC_SPC,
+        RCS(KC_M),  RCS(KC_O),  LCTL(KC_SPC)
     )
 };
